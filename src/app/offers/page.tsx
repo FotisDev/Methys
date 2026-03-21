7f755bff56e3a0c9212cd2d5698751c824f2351b
FotisDev <fotislir@outlook.com>
2026-03-21 16:43:36 +0200

    offers Metadata

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -1,11 +1,22 @@
-
 import { fetchOffers } from "@/_lib/backend/offers/actions";
 import { HeaderProvider } from "@/components/providers/HeaderProvider";
 import Footer from "@/components/footer/Footer";
 import OffersPageComponent from "@/components/pages/offerPage";
-
+import { Metadata } from "next";
+import { createMetadata } from "@/components/SEO/metadata";
 
 //SEO
+
+export async function generateMetadata(): Promise<Metadata> {
+  return createMetadata({
+    MetaTitle: "Limited Offers | YourSiteName",
+    MetaDescription:
+      "Explore our exclusive limited-time offers with up to 20% off on selected items.",
+    canonical: "/offers",
+    OpenGraphImageUrl:
+      "https://mpnjvzyymmtvgsrfgjjc.supabase.co/storage/v1/object/public/OpenGraphImages/about-us.jpg",
+  });
+}
 
 export default async function OfferPage() {
   const offers = await fetchOffers();
@@ -15,7 +26,7 @@ export default async function OfferPage() {
       <section className="padding-y padding-x bg-white">
         <OffersPageComponent offerProduct={offers} />
       </section>
-      <Footer  />
+      <Footer />
     </HeaderProvider>
   );
 }
